9069af3bca602c25d7b33c0d1967d3a2074fdfd5
Victoriia-Ripka <98872709+Victoriia-Ripka@users.noreply.github.com>
2022-05-27 10:25:08 +0000

    Deploying to gh-pages from @ Victoriia-Ripka/IceCream-team-project@ad19fc50fb5a832a7d539c3cac91a485ab1084e5 🚀

--- src.2b4b2ab4.js
+++ src.2b4b2ab4.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/IceCream-team-project/src.37b8d65e.js.map
+//# sourceMappingURL=/IceCream-team-project/src.2b4b2ab4.js.map
