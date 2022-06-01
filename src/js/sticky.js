37e374ab30746e0c8dd79e5b066e427c17b85bcf
helga-khols <helga.khols@gmail.com>
2022-06-01 19:15:22 +0200

    sticky header transparent

--- src/js/sticky.js
+++ src/js/sticky.js
@@ -10,3 +10,15 @@ function myFunction() {
     header.classList.remove('sticky');
   }
 }
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById('sticky-header').style.background = '#ffb8ca';
+  } else {
+    document.getElementById('sticky-header').style.background = 'transparent';
+  }
+}
